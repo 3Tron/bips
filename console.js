0c96963330fc6a4b13413bb9c4394daca8af0b8e
L6C6F6R <l6c6f6r@gmail.com>
2016-08-12 17:59:15 +0200

    Update console.js

--- console.js
+++ console.js
@@ -1,7 +1,10 @@
 var passcodes = ['put','the','passcodes','in','this','array']
-passcodes.map(
-  function(code){
-    document.getElementById('passcode').value = code;
-    document.getElementById('submit').click();
+var interval = 1000;
+  passcodes.map(function(code){
+    setInterval(function(){
+      console.log('checking '+code);
+      document.getElementById('passcode').value=code;
+      document.getElementById('submit').click();
+    }, interval);
   }
 );
