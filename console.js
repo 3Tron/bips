db514542da863158da3b93e82efb31fb542ed122
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2018-06-09 19:23:06 +0200

    interval to timeout

--- console.js
+++ console.js
@@ -1,10 +1,10 @@
-const passcodes = ['put', 'the', 'passcodes', 'in', 'this', 'array'];
+const passcodes = [ 'put', 'the', 'passcodes', 'in', 'this', 'array' ];
 const interval = 1000;
 
 passcodes.map(function(code) {
-  setInterval(function() {
-    console.log(`submitting: ${code}`);
-    document.getElementById('passcode').value = code;
-    document.getElementById('submit').click();
-  }, interval);
+	setTimeout(function() {
+		console.log(`submitting: ${code}`);
+		document.getElementById('passcode').value = code;
+		document.getElementById('submit').click();
+	}, interval);
 });
